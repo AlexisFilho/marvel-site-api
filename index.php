1426d259ff7df68a940ccd2741d31d697c5130f2
mateuscmtropical <mateuscmtropical@gmail.com>
2022-10-31 20:51:37 -0300

    Side bar com icones e links

--- index.php
+++ index.php
@@ -16,15 +16,12 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/slider.css">
     <link rel="stylesheet" href="css/nav-bar-on-side.css">
+    <link rel="stylesheet" href="css/icon.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"> -->
 
     <script src="https://kit.fontawesome.com/4af1129b29.js" crossorigin="anonymous"></script>
     <script src="js/nav-bar-on-side.js"></script>
-    
-    <!-- <script src="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"></script> -->
 </head>
 
 <body id="body-pd">
@@ -39,40 +36,37 @@
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
             <div> 
-                <a href="#" class="nav_logo"> 
+                <a href="home" class="nav_logo"> 
                     <i class='bx bx-layer nav_logo-icon'></i> 
-                    <span class="nav_logo-name">BBBootstrap</span> 
+                    <span class="nav_logo-name">Marvel-site</span> 
                 </a>
                 <div class="nav_list"> 
-                    <a href="#" class="nav_link active"> 
+                    <a href="home" class="nav_link active"> 
                         <i class='bx bx-grid-alt nav_icon'></i> 
-                        <span class="nav_name">Dashboard</span> 
+                        <span class="nav_name">Home</span> 
                     </a> 
-                    <a href="#" class="nav_link"> 
-                        <i class='bx bx-user nav_icon'></i> 
-                        <span class="nav_name">Users</span> 
-                    </a> <a href="#" class="nav_link"> 
-                        <i class='bx bx-message-square-detail nav_icon'></i> 
-                        <span class="nav_name">Messages</span> 
+                    <a href="personagens" class="nav_link"> 
+                        <img src="imagens/ironManAmerica.png" alt="" class="icon">
+                        <span class="nav_name">Personagens</span> 
                     </a> 
-                    <a href="#" class="nav_link"> 
-                        <i class='bx bx-bookmark nav_icon'></i> 
-                        <span class="nav_name">Bookmark</span> 
+                    <a href="quadrinhos" class="nav_link"> 
+                        <img src="imagens/comics.png" alt="" class="icon">
+                        <span class="nav_name">Quadrinhos</span> 
                     </a> 
-                    <a href="#" class="nav_link"> 
-                        <i class='bx bx-folder nav_icon'></i> 
-                        <span class="nav_name">Files</span> 
+                    <a href="sagas" class="nav_link"> 
+                        <img src="imagens/sagas.png" alt="" class="icon"> 
+                        <span class="nav_name">Sagas</span> 
                     </a> 
-                    <a href="#" class="nav_link"> 
-                        <i class='bx bx-bar-chart-alt-2 nav_icon'></i> 
-                        <span class="nav_name">Stats</span> 
+                    <a href="series" class="nav_link"> 
+                        <img src="imagens/homemDeFerro.png" alt="" class="icon">
+                        <span class="nav_name">Séries</span> 
+                    </a> 
+                    <a href="criadores" class="nav_link"> 
+                        <img src="imagens/criador.png" alt="" class="icon"> 
+                        <span class="nav_name">Criadores</span> 
                     </a> 
                 </div>
-            </div> 
-            <a href="#" class="nav_link"> 
-                <i class='bx bx-log-out nav_icon'></i> 
-                <span class="nav_name">SignOut</span> 
-            </a>
+            </div>
         </nav>
     </div>
 
